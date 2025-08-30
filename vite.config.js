062b0535f32ca675affdc9f677f2349f0d8088da
Yusuf-77-tech <yusuflaway09@gmail.com>
2025-08-30 22:33:14 +0100

    fix: removed external react-router-dom from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Add this line if it doesn't exist yet
-      external: ['react-router-dom']
-    }
-  }
+  
 });
